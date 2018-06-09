8c7b4ae7d59141c5da846f8bed4a9e3bbc892ac0
Yucked <mail@dvlp.nill.sh>
2018-06-09 16:16:54 -0700

    Keyword object

---
+++ Objects/KeywordObject.cs
@@ -0,0 +1,21 @@
+﻿namespace Wit.Net.Objects
+{
+    using Newtonsoft.Json;
+    public class KeywordObject
+    {
+        [JsonProperty("builtin")]
+        public bool Builtin { get; internal set; }
+        [JsonProperty("doc")]
+        public string Description { get; internal set; }
+        [JsonProperty("exotic")]
+        public bool Exotic { get; internal set; }
+        [JsonProperty("id")]
+        public string Id { get; internal set; }
+        [JsonProperty("lang")]
+        public string Language { get; internal set; }
+        [JsonProperty("lookups")]
+        public string[] Lookups { get; internal set; }
+        [JsonProperty("name")]
+        public string Name { get; internal set; }
+    }
+}

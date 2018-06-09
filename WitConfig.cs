0616a6022d0c690ef50c9a7b16b29ef529e9aeca
Yucked <mail@dvlp.nill.sh>
2018-06-09 09:20:31 -0700

    Simple Config

---
+++ WitConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Wit.Net
+{
+    /// <summary>Configuration for  Wit client.</summary>
+    public class WitConfig
+    {
+        /// <summary>Your server access token.</summary>       
+        public string AccessToken { get; set; }
+        /// <summary>Throw when an exception occurs or log them in <see cref="Log"/> event. Some exceptions won't be swallowed.</summary>        
+        public bool ThrowOnError { get; set; }        
+    }
+}

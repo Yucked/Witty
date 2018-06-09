c837fc35959515604581409e66b8644c0e18b71d
Yucked <mail@dvlp.nill.sh>
2018-06-09 10:41:19 -0700

    um wtf

--- WitConfig.cs
+++ WitConfig.cs
@@ -3,9 +3,17 @@
     /// <summary>Configuration for  Wit client.</summary>
     public class WitConfig
     {
-        /// <summary>Your server access token.</summary>       
+        /// <summary>Your server access token.</summary>
         public string AccessToken { get; set; }
-        /// <summary>Throw when an exception occurs or log them in <see cref="Log"/> event. Some exceptions won't be swallowed.</summary>        
-        public bool ThrowOnError { get; set; }        
+        /// <summary>What kind of event logging do you want?</summary>
+        public LogSeverity LogSeverity { get; set; } = LogSeverity.INFO;
+    }
+    /// <summary>LogSeverity.</summary>
+    public enum LogSeverity
+    {
+        /// <summary>Info with silent exceptions if any.</summary>
+        INFO,
+        /// <summary>Exceptions Only. No Info.</summary>
+        EXCEPTIONS
     }
 }

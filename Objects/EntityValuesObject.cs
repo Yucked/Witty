f2e9060473690ff0f915b0bbfdc441432e122a7a
Yucked <mail@dvlp.nill.sh>
2018-06-09 13:06:26 -0700

    Kek

---
+++ Objects/EntityValuesObject.cs
@@ -0,0 +1,28 @@
+﻿namespace Wit.Net.Objects
+{
+    using Newtonsoft.Json;
+    public class EntityValuesObject
+    {
+        [JsonProperty("builtin")]
+        public bool Builtin { get; internal set; }
+        [JsonProperty("doc")]
+        public string Description { get; internal set; }
+        [JsonProperty("id")]
+        public string Id { get; internal set; }
+        [JsonProperty("lang")]
+        public string Language { get; internal set; }
+        [JsonProperty("lookups")]
+        public string[] Lookups { get; internal set; }
+        [JsonProperty("name")]
+        public string Name { get; internal set; }
+        [JsonProperty("values")]
+        public Values[] Values { get; internal set; }
+    }
+    public partial class Values
+    {
+        [JsonProperty("value")]
+        public string Value { get; internal set; }
+        [JsonProperty("expressions")]
+        public string[] Expressions { get; internal set; }
+    }
+}

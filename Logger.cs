b21c754c357d7ff0beced38ec180d19d292e9541
Yucked <mail@dvlp.nill.sh>
2018-06-10 13:23:39 -0700

    Global Log

---
+++ Logger.cs
@@ -0,0 +1,18 @@
+﻿namespace Wit.Net
+{
+    using System;
+    public sealed class Logger
+    {
+        internal Logger() { }
+        public event Action<string, Exception> Log;
+        internal static Logger Logging { get; } = new Logger();
+        internal void Send(string message = null, Exception exception = null)
+        {
+            switch (WitClient.Config.LogSeverity)
+            {
+                case LogSeverity.EXCEPTIONS: throw exception;
+                case LogSeverity.INFO: Log?.Invoke(message, exception); break;
+            }
+        }
+    }
+}

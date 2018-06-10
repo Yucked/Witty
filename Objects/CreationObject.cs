9533dbe9e7df5095e9eb63f6155aef27704774ca
Yucked <mail@dvlp.nill.sh>
2018-06-10 16:43:28 -0700

    Creation object

---
+++ Objects/CreationObject.cs
@@ -0,0 +1,11 @@
+﻿namespace Wit.Net.Objects
+{
+    using Newtonsoft.Json;
+    public class CreationObject
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; internal set; }
+        [JsonProperty("app_id")]
+        public string AppId { get; internal set; }
+    }
+}

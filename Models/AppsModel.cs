25dba5a148049fa9d1ed6c0b0c996f395cd99b10
Yucked <mail@dvlp.nill.sh>
2018-06-10 13:32:31 -0700

    internal set;

---
+++ Models/AppsModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Wit.Net.Models
+{
+    using System;
+    using Newtonsoft.Json;
+    public class AppsModel
+    {
+        [JsonProperty("name")]
+        public string Name { get; internal set; }
+        [JsonProperty("id")]
+        public string Id { get; internal set; }
+        [JsonProperty("description")]
+        public string Description { get; internal set; }
+        [JsonProperty("lang")]
+        public string Language { get; internal set; }
+        [JsonProperty("private")]
+        public string Private { get; internal set; }
+        [JsonProperty("created_at")]
+        public DateTimeOffset CreatedAt { get; internal set; }
+    }
+}

a031f501949648aaaf4419f6a0be363829a1ed0b
Yucked <mail@dvlp.nill.sh>
2018-06-10 17:56:59 -0700

    AppModel and Languages

---
+++ Models/AppModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Wit.Net.Models
+{
+    using Newtonsoft.Json;
+    internal class AppModel
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("private")]
+        public bool IsPrivate { get; set; }
+        [JsonProperty("lang")]
+        public string Language { get; set; }
+        [JsonProperty("desc")]
+        public string Description { get; set; }
+    }
+}
---
+++ Models/Language.cs
@@ -0,0 +1,74 @@
+﻿namespace Wit.Net.Models
+{
+    public enum Language
+    {
+        /// <summary>English</summary>
+        ENG,
+        /// <summary>French</summary>
+        FRE,
+        /// <summary>German</summary>
+        GEM,
+        /// <summary>Italian</summary>
+        ITA,
+        /// <summary>Dutch</summary>
+        NL,
+        /// <summary>Spanish</summary>
+        ES,
+        /// <summary>Polish</summary>
+        PL,
+        /// <summary>Swedish</summary>
+        SV,
+        /// <summary>Portuguese</summary>
+        PT,
+        /// <summary>Russian</summary>
+        RU,
+
+
+
+        /// <summary>Indonesia</summary>
+        ID,
+        /// <summary>Arabic</summary>
+        AR,
+        /// <summary>Turkish</summary>
+        TR,
+        /// <summary>Thai</summary>
+        TH,
+        /// <summary>Vietnamese</summary>
+        VI,
+
+        /// <summary>Japanese</summary>
+        JA,
+        /// <summary>Chinese Simplified</summary>
+        ZHCN,
+        /// <summary>Chinese Traditional</summary>
+        ZHTW,        
+        /// <summary>Korean</summary>
+        KO,        
+        /// <summary>Romanian</summary>
+        RO,
+        /// <summary>Hungarian</summary>
+        HU,        
+        /// <summary>Czech</summary>
+        CS,
+        /// <summary>Hindi</summary>
+        HI,
+        /// <summary>Bengali</summary>
+        BN,
+        /// <summary>Danish</summary>
+        DA,
+        /// <summary>Farsi</summary>
+        FA,
+        /// <summary>Flilipino</summary>
+        TL,
+        /// <summary>Finnish</summary>
+        FI,
+        /// <summary>Hebrew</summary>
+        IW,
+        /// <summary>Malay</summary>
+        MS,
+        /// <summary>Norwegian</summary>
+        NO,
+        /// <summary>Ukrainian</summary>
+        UK
+    }
+}

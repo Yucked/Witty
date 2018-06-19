4a90f7e6fe81c330cf5c64be96cee3ac8905dc01
Yucked <mail@dvlp.nill.sh>
2018-06-19 15:54:31 -0700

    Audio Type for speech

---
+++ Models/AudioType.cs
@@ -0,0 +1,18 @@
+﻿namespace WitSharp.Models
+{
+    public enum AudioType
+    {
+        /// <summary>
+        /// RIFF WAVE is supported. (Linear, Little-Endian)
+        /// </summary>
+        WAV,
+        /// <summary>
+        /// For MP3 File or Stream.
+        /// </summary>
+        MPEG3,
+        /// <summary>
+        /// For G.711 U-Law or stream. Sampling rate must bt 8khz.
+        /// </summary>
+        ULAW
+    }
+}

8b194ad005f9536c8a07f0d38908f90e6766ca32
Yucked <mail@dvlp.nill.sh>
2018-06-19 16:50:58 -0700

    All languages added that are supported by the API

--- Models/Language.cs
+++ Models/Language.cs
@@ -2,73 +2,130 @@
 {
     public enum Language
     {
-        /// <summary>English</summary>
-        En,
-        /// <summary>French</summary>
-        Fr,
-        /// <summary>German</summary>
-        Gem,
-        /// <summary>Italian</summary>
-        Ita,
-        /// <summary>Dutch</summary>
-        Nl,
-        /// <summary>Spanish</summary>
-        Es,
-        /// <summary>Polish</summary>
-        Pl,
-        /// <summary>Swedish</summary>
-        Sv,
-        /// <summary>Portuguese</summary>
-        Pt,
-        /// <summary>Russian</summary>
-        Ru,
-
-
-
-        /// <summary>Indonesia</summary>
-        Id,
-        /// <summary>Arabic</summary>
-        Ar,
-        /// <summary>Turkish</summary>
-        Tr,
-        /// <summary>Thai</summary>
-        Th,
-        /// <summary>Vietnamese</summary>
-        Vi,
-
-        /// <summary>Japanese</summary>
-        Ja,
-        /// <summary>Chinese Simplified</summary>
-        Zhcn,
-        /// <summary>Chinese Traditional</summary>
-        Zhtw,        
-        /// <summary>Korean</summary>
-        Ko,        
-        /// <summary>Romanian</summary>
-        Ro,
-        /// <summary>Hungarian</summary>
-        Hu,        
-        /// <summary>Czech</summary>
-        Cs,
-        /// <summary>Hindi</summary>
-        Hi,
-        /// <summary>Bengali</summary>
-        Bn,
-        /// <summary>Danish</summary>
-        Da,
-        /// <summary>Farsi</summary>
-        Fa,
-        /// <summary>Flilipino</summary>
-        Tl,
-        /// <summary>Finnish</summary>
-        Fi,
-        /// <summary>Hebrew</summary>
-        Iw,
-        /// <summary>Malay</summary>
-        Ms,
-        /// <summary>Norwegian</summary>
-        No,
-        /// <summary>Ukrainian</summary>
-        Uk
+        /// <summary>
+        /// English
+        /// </summary>
+        EN,
+        /// <summary>
+        /// French
+        /// </summary>
+        FR,
+        /// <summary>
+        /// German
+        /// </summary>
+        DE,
+        /// <summary>
+        /// Italian
+        /// </summary>
+        IT,
+        /// <summary>
+        /// Dutch
+        /// </summary>
+        DL,
+        /// <summary>
+        /// Spanish
+        /// </summary>
+        ES,
+        /// <summary>
+        /// Polish
+        /// </summary>
+        PL,
+        /// <summary>
+        /// Swedish
+        /// </summary>
+        SV,
+        /// <summary>
+        /// Portuguese
+        /// </summary>
+        PT,
+        /// <summary>
+        /// Estonian
+        /// </summary>
+        ET,
+        /// <summary>
+        /// Russian
+        /// </summary>
+        RU,
+        /// <summary>
+        /// Albanian
+        /// </summary>
+        SQ,
+        /// <summary>
+        /// Croatian
+        /// </summary>
+        HR,
+        /// <summary>
+        /// Icelandic
+        /// </summary>
+        IS,
+        /// <summary>
+        /// Persian
+        /// </summary>
+        FA,
+        /// <summary>
+        /// Turkish
+        /// </summary>
+        TR,
+        /// <summary>
+        /// Arabic
+        /// </summary>
+        AR,
+        /// <summary>
+        /// Czech
+        /// </summary>
+        CS,
+        /// <summary>
+        /// Indonesian
+        /// </summary>
+        ID,
+        /// <summary>
+        /// Romanian
+        /// </summary>
+        RO,
+        /// <summary>
+        /// Ukrainian
+        /// </summary>
+        UK,
+        /// <summary>
+        /// Azerbaijani
+        /// </summary>
+        AZ,
+        /// <summary>
+        /// Danish
+        /// </summary>
+        DA,
+        /// <summary>
+        /// Japanese
+        /// </summary>
+        JA,
+        /// <summary>
+        /// Serbian
+        /// </summary>
+        SR,
+        VI,
+        BN,
+        FI,
+        KO,
+        SK,
+        BS,
+        KA,
+        LA,
+        SL,
+        BG,
+        EL,
+        LT,
+        SW,
+        MY,
+        HE,
+        MK,
+        TL,
+        CA,
+        HI,
+        MS,
+        TA,
+        ZH,
+        HU,
+        NO,
+        TH
     }
 }

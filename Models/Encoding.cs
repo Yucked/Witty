0b42cc911a1867e557d26e4e848dcc30a9568710
Yucked <mail@dvlp.nill.sh>
2018-06-19 16:05:27 -0700

    Encoding for raw audio file

---
+++ Models/Encoding.cs
@@ -0,0 +1,14 @@
+﻿namespace WitSharp.Models
+{
+    public enum Encoding
+    {
+        Signed_Integer,
+        Unsigned_Integer,
+        Floating_Point,
+        Mu_Law,
+        A_Law,
+        Ima_AdPCM,
+        Ms_AdPCM,
+        GSM_FullRate
+    }
+}

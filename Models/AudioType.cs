c39f6ea01a274730f8dbeb39788daa026584e21f
Yucked <mail@dvlp.nill.sh>
2018-06-25 18:31:53 -0400

    Name Fixes

--- Models/AudioType.cs
+++ Models/AudioType.cs
@@ -5,14 +5,14 @@
         /// <summary>
         /// RIFF WAVE is supported. (Linear, Little-Endian)
         /// </summary>
-        WAV,
+        Wav,
         /// <summary>
         /// For MP3 File or Stream.
         /// </summary>
-        MPEG3,
+        Mpeg3,
         /// <summary>
         /// For G.711 U-Law or stream. Sampling rate must bt 8khz.
         /// </summary>
-        ULAW
+        Ulaw
     }
 }
